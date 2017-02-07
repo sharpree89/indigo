f3cfc67f3df91fbcda79da11065b5d8ba0b6e587
Preeya Sharma <sharpree89@gmail.com>
2017-02-06 21:12:09 -0800

    navigation changes, more css

--- server.py
+++ server.py
@@ -2,35 +2,17 @@ from flask import Flask, render_template, redirect
 
 app = Flask(__name__, static_url_path='/static')
 
-# HOME
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# BUTTONS
-
 @app.route('/components')
-def buttons_home():
+def components():
     return render_template('components.html')
 
-@app.route('/components/buttons')
-def buttons():
-    return render_template('buttons.html')
-
-@app.route('/components/tags')
-def tags():
-    return render_template('tags.html')
-
-# LABELS
-
-@app.route('/components/stickys')
-def stickys():
-    return render_template('stickys.html')
-
-@app.route('/components/badges')
-def badges():
-    return render_template('badges.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
 app.run(debug=True)
