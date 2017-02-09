837a9bb478ba1e9cd21166706d542d535218e390
Preeya Sharma <sharpree89@gmail.com>
2017-02-08 22:13:46 -0800

    added tables and lists, more components page formatting, tweaked navbar

--- static/js/app.js
+++ static/js/app.js
@@ -16,7 +16,7 @@
 
   /* Set the width of the side navigation to 250px */
   function openNav() {
-      document.getElementById("mySidenav").style.width = "250px";
+      document.getElementById("mySidenav").style.width = "225px";
   }
 
   /* Set the width of the side navigation to 0 */
@@ -26,8 +26,8 @@
 
   /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
   function openNav() {
-      document.getElementById("mySidenav").style.width = "250px";
-      document.getElementById("main").style.marginLeft = "250px";
+      document.getElementById("mySidenav").style.width = "225px";
+      document.getElementById("main").style.marginLeft = "225px";
   }
 
   /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
