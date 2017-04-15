59ffdf6f52f0d4063d78fa03d5433aad95578b3d
Preeya Sharma <sharpree89@gmail.com>
2017-04-15 14:44:18 -0700

    added drop down menus

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,8 @@
 
+
+  //=========== NAVIGATION SCRIPTS ===========//
+
+
   /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
   function myFunction() {
 
