ba97b41a2a24535699844eed2e0670eaed7d95e1
Preeya Sharma <sharpree89@gmail.com>
2017-04-15 15:53:08 -0700

    replaced side nav with dropdown menu, tweaked JS

--- static/js/app.js
+++ static/js/app.js
@@ -18,24 +18,12 @@
     }
   }
 
-  /* Set the width of the side navigation to 250px */
-  function openNav() {
-      document.getElementById("mySidenav").style.width = "225px";
-  }
 
-  /* Set the width of the side navigation to 0 */
-  function closeNav() {
-      document.getElementById("mySidenav").style.width = "0";
-  }
+  //=========== DROPDOWN MENU SCRIPTS ===========//
 
-  /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
-  function openNav() {
-      document.getElementById("mySidenav").style.width = "225px";
-      document.getElementById("main").style.marginLeft = "225px";
-  }
 
-  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-  function closeNav() {
-      document.getElementById("mySidenav").style.width = "0";
-      document.getElementById("main").style.marginLeft = "0";
-  }
+  $(document).ready(function() {
+    $(".dropdown").click( function () {
+      $(".content", this).toggle();
+    });
+  });
