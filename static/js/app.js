ad23c97fb02b861095db09330e56bef84a64cd0f
Preeya Sharma <sharpree89@gmail.com>
2017-04-17 17:59:49 -0700

    navbar now has integrated dropdown menus, simplified navbar classes

--- static/js/app.js
+++ static/js/app.js
@@ -7,12 +7,4 @@
       $("ul.content", this).toggle();
     });
 
-    //=========== NAVIGATION SCRIPTS ===========//
-
-    jQuery('.toggle-nav').click(function(e) {
-        jQuery(this).toggleClass('active-mobile');
-        jQuery('.menu-mobile div').toggleClass('active-mobile');
-
-        e.preventDefault();
-    });
   });
