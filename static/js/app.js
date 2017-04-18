b05ce66606bf012db309cc2fa7b46d8cd3aedca8
Preeya Sharma <sharpree89@gmail.com>
2017-04-18 00:27:44 -0700

    added responsive navbar

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,23 @@
 
   $(document).ready(function() {
 
-    //=========== DROPDOWN MENU SCRIPTS ===========//
+    //=========== DROPDOWN MENU SCRIPT ===========//
 
-    $(".trigger").click( function () {
+    $(".trigger").click(function () {
       $("ul.content", this).toggle();
+    });
+
+    //=========== RESPONSIVE NAVBAR SCRIPT ===========//
+
+    $("#nav-mobile").html($("#navbar").html());
+    $("#nav-trigger span").click(function(){
+        if ($("ul.navbar").hasClass("expanded")) {
+            $("ul.navbar").removeClass("expanded").slideUp(500);
+            $(this).removeClass("open");
+        } else {
+            $("ul.navbar").addClass("expanded").slideDown(500);
+            $(this).addClass("open");
+        }
     });
 
   });
